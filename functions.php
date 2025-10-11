9a996149fcc76c33b4ab39ff9677f8c7cfcf9e4e
alexy103 <alexy103@hotmail.fr>
2025-10-11 19:12:13 +0200

    fix css

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ add_action('after_setup_theme', function () {
 });
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('aka-style', get_template_directory_uri() . '/css/style.css?version=23456');
+    wp_enqueue_style('aka-style', get_template_directory_uri() . '/css/style.css?version=234567');
 });
 
 add_action('wp_enqueue_scripts', function () {
