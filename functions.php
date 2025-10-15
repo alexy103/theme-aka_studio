9b3db73a1f64e70d87bd98e695f3b914a2ce1210
alexy103 <alexy103@hotmail.fr>
2025-10-15 22:28:05 +0200

    fix

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ add_action('after_setup_theme', function () {
 });
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('aka-style', get_template_directory_uri() . '/css/style.css?version=2344584576789701234');
+    wp_enqueue_style('aka-style', get_template_directory_uri() . '/css/style.css?version=23445864576789701234');
 });
 
 add_action('wp_enqueue_scripts', function () {
@@ -17,7 +17,7 @@ add_action('wp_enqueue_scripts', function () {
 
     wp_enqueue_script('theme-index', $theme_js_path . 'index.js', [], null, true);
     wp_enqueue_script('theme-menu', $theme_js_path . 'menu.js', [], null, true);
-    wp_enqueue_script('theme-navigation', $theme_js_path . 'navigation.js?version=255474845787555', [], null, true);
+    wp_enqueue_script('theme-navigation', $theme_js_path . 'navigation.js?version=2554748645787555', [], null, true);
     wp_enqueue_script('theme-about', $theme_js_path . 'about.js', [], null, true);
     wp_enqueue_script('theme-work', $theme_js_path . 'work.js', [], null, true);
     wp_enqueue_script('theme-contact', $theme_js_path . 'contact.js', [], null, true);
