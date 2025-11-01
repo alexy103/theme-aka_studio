c506a49b2c671d61b2eee8631572c73a9636df21
Delaporte Alexy <alexy.delaporte@etudiant.univ-lr.fr>
2025-11-01 12:48:35 +0100

    try fix

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ add_action('after_setup_theme', function ()
 
 add_action('wp_enqueue_scripts', function ()
 {
-    wp_enqueue_style('aka-style', get_template_directory_uri() . '/css/style.css?version=8899');
+    wp_enqueue_style('aka-style', get_template_directory_uri() . '/css/style.css?version=88995');
 });
 
 add_action('wp_enqueue_scripts', function ()
